77761848b253c4ebb55443c6a01b03c092fb0161
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-07 17:13:22 +0200

    feat: Reown initial implementation

--- src/lib/wrappers.tsx
+++ src/lib/wrappers.tsx
@@ -1,24 +1,56 @@
 import { NextUIProvider } from '@nextui-org/system';
+import { createAppKit } from '@reown/appkit';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+import { arbitrum, mainnet, sepolia } from '@reown/appkit/networks';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
-import { createConfig, http, WagmiProvider } from 'wagmi';
-import { arbitrum, sepolia } from 'wagmi/chains';
+import { WagmiProvider } from 'wagmi';
 import { AuthenticationProvider } from './authentication';
 
-const wagmiConfig = createConfig({
-    chains: [arbitrum, sepolia],
-    transports: {
-        [arbitrum.id]: http(),
-        [sepolia.id]: http(),
-    },
-});
+// const wagmiConfig = createConfig({
+//     chains: [arbitrum, sepolia],
+//     transports: {
+//         [arbitrum.id]: http(),
+//         [sepolia.id]: http(),
+//     },
+// });
 
 const queryClient = new QueryClient();
+
+const projectId = '6fb791d3d18d57d28ae7677e4cff8c6e';
+
+const metadata = {
+    name: 'Ratio1',
+    description: 'Ratio1',
+    url: 'https://example.com', // Origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/179229932'],
+};
+
+const wagmiAdapter = new WagmiAdapter({
+    networks: [arbitrum, sepolia, mainnet],
+    projectId,
+    ssr: false,
+});
+
+createAppKit({
+    adapters: [wagmiAdapter],
+    networks: [arbitrum, sepolia, mainnet],
+    projectId,
+    metadata,
+    features: {
+        analytics: false,
+    },
+    themeMode: 'light',
+    themeVariables: {
+        '--w3m-font-family': 'Mona Sans',
+        '--w3m-accent': '#1b47f7',
+    },
+});
 
 export function Wrappers({ children }: { children: React.ReactNode }) {
     return (
         <BrowserRouter>
-            <WagmiProvider config={wagmiConfig}>
+            <WagmiProvider config={wagmiAdapter.wagmiConfig}>
                 <QueryClientProvider client={queryClient}>
                     <NextUIProvider>
                         <AuthenticationProvider>{children}</AuthenticationProvider>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,19 @@
 import { getShortAddress } from '@lib/utils';
-import { useAccount, useEnsName } from 'wagmi';
+import { useAccount, useDisconnect, useEnsName } from 'wagmi';
 
 function Profile() {
     const { address } = useAccount();
     const { data, error, status } = useEnsName({ address });
+    const { disconnect } = useDisconnect();
 
     return (
         <div className="center-all flex-col gap-4 font-medium">
             <div>Profile & KYC</div>
+
+            <appkit-button />
+            <appkit-account-button />
+            <appkit-connect-button />
+            <appkit-network-button />
 
             {!!address && (
                 <div className="rounded-full bg-body px-2.5 py-0.5 text-[15px] font-medium text-white">
@@ -16,6 +22,10 @@ function Profile() {
             )}
 
             {!!data && <div>{data}</div>}
+
+            <button onClick={() => disconnect()} className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600">
+                Disconnect Wallet
+            </button>
         </div>
     );
 }
