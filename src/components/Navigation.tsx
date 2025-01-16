e26e4381c715689242e5f691d7909b047d9df918
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-16 21:08:14 +0200

    chore: Use Link in Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { mainRoutesInfo, ParentRoute, routes, SimpleRoute } from '@lib/routes';
 import clsx from 'clsx';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Navigation() {
     return (
@@ -18,21 +18,20 @@ export default function Navigation() {
 
 function Route({ route }: { route: SimpleRoute | ParentRoute }) {
     const location = useLocation();
-    const navigate = useNavigate();
 
     return (
         <div className="flex flex-col gap-2">
-            <div
+            <Link
+                to={route.path}
                 className={clsx('min-w-40 cursor-pointer rounded-lg px-3 py-2.5 transition-all hover:bg-[#e2eefb]', {
                     'bg-[#e2eefb] text-primary': location.pathname.includes(route.path),
                 })}
-                onClick={() => navigate(route.path)}
             >
                 <div className="flex items-center gap-2.5">
                     <div className="text-[22px]">{route.icon}</div>
                     <div className="text-[15px] font-medium"> {mainRoutesInfo[route.path].title}</div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 }
--- src/pages/T&C.tsx
+++ src/pages/T&C.tsx
@@ -2,7 +2,7 @@ function TermsAndConditions() {
     return (
         <div className="col center-all gap-6">
             <div className="text-justify">
-                <p className="mb-3">
+                <p className="mb-4">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec lacinia mauris. Vivamus malesuada urna
                     venenatis fringilla pellentesque. Donec congue laoreet enim, vel lacinia libero volutpat nec. Cras et mauris
                     enim. Phasellus porttitor ex quis sapien iaculis condimentum. Quisque mollis lectus ut odio pharetra
@@ -12,7 +12,7 @@ function TermsAndConditions() {
                     mauris enim sit amet metus.
                 </p>
 
-                <p className="mb-3">
+                <p className="mb-4">
                     Cras semper risus id dui condimentum, quis convallis lectus auctor. Duis consequat ligula placerat lorem
                     lacinia, eu placerat ex molestie. In pharetra urna sit amet efficitur efficitur. Vestibulum mollis ipsum nec
                     tellus porta varius. Donec viverra justo nulla, eget efficitur lectus feugiat ac. Suspendisse et malesuada
@@ -21,7 +21,7 @@ function TermsAndConditions() {
                     vulputate justo vitae dignissim.
                 </p>
 
-                <p className="mb-3">
+                <p className="mb-4">
                     Maecenas commodo dolor lacus, nec varius ligula pulvinar vitae. Pellentesque habitant morbi tristique
                     senectus et netus et malesuada fames ac turpis egestas. Duis ornare hendrerit leo sit amet vulputate.
                     Praesent pharetra facilisis velit, in semper nulla accumsan quis. Pellentesque dui ipsum, posuere dignissim
@@ -33,7 +33,7 @@ function TermsAndConditions() {
                     nulla consectetur.
                 </p>
 
-                <p className="mb-3">
+                <p className="mb-4">
                     Nunc id varius enim. Ut a faucibus est. Nullam malesuada lorem eros, eget varius libero malesuada vitae.
                     Praesent suscipit enim sed risus tempor congue. Maecenas tristique mattis condimentum. Donec et sollicitudin
                     nisl. Quisque pharetra diam vitae enim ornare suscipit. Mauris semper eros lorem.
