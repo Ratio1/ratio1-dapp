4634705a02b8601b7acffef1a5d70c994f638496
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-24 13:40:21 +0200

    style: MobileTabs font weight testing

--- src/components/Mobile/MobileTabs.tsx
+++ src/components/Mobile/MobileTabs.tsx
@@ -10,7 +10,7 @@ export default function MobileTabs() {
 
     return (
         <div className="center-all fixed bottom-0 left-0 right-0 z-40">
-            <div className="row w-full justify-around gap-4 border-t-2 border-slate-200 bg-slate-100 px-2.5 py-2.5">
+            <div className="center-all w-full gap-2 border-t-2 border-slate-200 bg-slate-100 px-2.5 py-2.5">
                 {mobileRoutes.map((route, index) => (
                     <div key={route.path}>
                         <Link
@@ -23,59 +23,11 @@ export default function MobileTabs() {
                             )}
                         >
                             <div className="text-[26px]">{route.icon}</div>
-                            <div className="text-sm font-medium">{mobileTitles[index]}</div>
+                            <div className="text-sm font-semibold">{mobileTitles[index]}</div>
                         </Link>
                     </div>
                 ))}
             </div>
-
-            {/* <div className="row w-full justify-around gap-4 border-t-2 border-slate-200 bg-slate-100 px-2.5 py-2.5">
-                {mobileRoutes.map((route) => (
-                    <div key={route.path}>
-                        <Link to={route.path}>
-                            <div
-                                className={clsx('cursor-pointer p-3.5 text-3xl text-slate-500 hover:opacity-70', {
-                                    '!text-primary': location.pathname.includes(route.path),
-                                })}
-                            >
-                                {route.icon}
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-            </div> */}
-
-            {/* <div className="row mb-2.5 justify-around gap-4 rounded-full bg-body px-2.5 py-2.5">
-                {mobileRoutes.map((route) => (
-                    <div key={route.path}>
-                        <Link to={route.path}>
-                            <div
-                                className={clsx('cursor-pointer rounded-full p-3.5 text-2xl text-white hover:opacity-70', {
-                                    'bg-white !text-body !opacity-100': location.pathname.includes(route.path),
-                                })}
-                            >
-                                {route.icon}
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-            </div> */}
-
-            {/* <div className="row mb-2.5 justify-around gap-4 rounded-full bg-primary px-2.5 py-2.5">
-                {mobileRoutes.map((route) => (
-                    <div key={route.path}>
-                        <Link to={route.path}>
-                            <div
-                                className={clsx('cursor-pointer rounded-full p-3.5 text-2xl text-white hover:opacity-70', {
-                                    'bg-white !text-primary !opacity-100': location.pathname.includes(route.path),
-                                })}
-                            >
-                                {route.icon}
-                            </div>
-                        </Link>
-                    </div>
-                ))}
-            </div> */}
         </div>
     );
 }
