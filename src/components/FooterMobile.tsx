1771bbc87175b5c0859c1ff780493128149aab75
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-27 19:24:43 +0200

    chore: Add api status in the mobile footer

--- src/components/FooterMobile.tsx
+++ src/components/FooterMobile.tsx
@@ -1,5 +1,9 @@
 import Logo from '@assets/logo_blue.svg';
+import { ping } from '@lib/api/backend';
 import { mainRoutesInfo, routePath } from '@lib/routes';
+import { Spinner } from '@nextui-org/spinner';
+import { useQuery } from '@tanstack/react-query';
+import clsx from 'clsx';
 import { RiDiscordLine, RiLinkedinBoxLine, RiTwitterXLine, RiYoutubeLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
@@ -11,6 +15,11 @@ const socialLinks = [
 ];
 
 function FooterMobile() {
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['ping'],
+        queryFn: ping,
+    });
+
     return (
         <div className="col center-all w-full gap-10 rounded-3xl bg-lightAccent px-8 py-12">
             <div className="col gap-4">
@@ -43,6 +52,23 @@ function FooterMobile() {
                 <Link to={routePath.privacyPolicy} className="hover:opacity-70">
                     {mainRoutesInfo[routePath.privacyPolicy].title}
                 </Link>
+            </div>
+
+            <div className="row mx-auto gap-2 rounded-lg bg-[#e8ebf6] px-3.5 py-2.5">
+                <div className="center-all">
+                    {isLoading ? (
+                        <Spinner size="sm" className="scale-75" />
+                    ) : (
+                        <div
+                            className={clsx('h-2.5 w-2.5 rounded-full', {
+                                'bg-green-500': !error,
+                                'bg-red-500': data?.status === 'error' || !!error,
+                            })}
+                        ></div>
+                    )}
+                </div>
+
+                <div className="text-sm font-medium text-slate-600">API Status</div>
             </div>
         </div>
     );
