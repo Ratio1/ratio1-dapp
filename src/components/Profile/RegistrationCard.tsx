285cf5658b8f48c7473a408f265bacd6bf2f30b4
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-29 19:08:08 +0200

    feat: Email registration

--- src/components/Profile/RegistrationCard.tsx
+++ src/components/Profile/RegistrationCard.tsx
@@ -1,3 +1,5 @@
+import { registerEmail } from '@lib/api/backend';
+import { Alert } from '@nextui-org/alert';
 import { Button } from '@nextui-org/button';
 import { Form } from '@nextui-org/form';
 import { Input } from '@nextui-org/input';
@@ -6,17 +8,23 @@ import { Switch } from '@nextui-org/switch';
 import { Card } from '@shared/Card';
 import { DetailedAlert } from '@shared/DetailedAlert';
 import { Label } from '@shared/Label';
+import { QueryObserverResult, RefetchOptions } from '@tanstack/react-query';
 import { ApiAccount } from '@typedefs/blockchain';
 import { RegistrationStatus } from '@typedefs/profile';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 import { RiMailLine, RiMailSendLine } from 'react-icons/ri';
 
 function RegistrationCard({
     account,
     getRegistrationStatus,
+    fetchAccount,
+    setAccount,
 }: {
     account?: ApiAccount;
     getRegistrationStatus: () => RegistrationStatus;
+    fetchAccount: (options?: RefetchOptions) => Promise<QueryObserverResult<ApiAccount, Error>>;
+    setAccount: React.Dispatch<React.SetStateAction<ApiAccount | undefined>>;
 }) {
     const [email, setEmail] = useState<string>('');
     const [isSelected, setSelected] = useState(true);
@@ -25,10 +33,23 @@ function RegistrationCard({
 
     const [isLoading, setLoading] = useState<boolean>(false);
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        console.log('register', email, isSelected);
-        onOpen();
+        console.log('Register', email, isSelected);
+
+        setLoading(true);
+
+        try {
+            const accountResponse: ApiAccount = await registerEmail({ email, receiveUpdates: isSelected });
+            setAccount(accountResponse);
+
+            onOpen();
+        } catch (error) {
+            console.error('Error', error);
+            toast.error('Unexpected error, please try again.');
+        } finally {
+            setLoading(false);
+        }
     };
 
     if (!account) {
@@ -43,39 +64,68 @@ function RegistrationCard({
                 label={getRegistrationStatus() === RegistrationStatus.NOT_REGISTERED ? <Label text="Not Registered" /> : <></>}
             >
                 <div className="flex h-full w-full items-center justify-between">
-                    <Form className="w-full" validationBehavior="native" onSubmit={onSubmit}>
-                        <div className="col w-full gap-4">
-                            <div className="flex w-full gap-2">
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    value={email}
-                                    onValueChange={setEmail}
-                                    size="md"
-                                    classNames={{
-                                        inputWrapper: 'bg-[#fcfcfd] border rounded-lg',
-                                        input: 'font-medium rounded-lg',
-                                    }}
-                                    variant="bordered"
-                                    color="primary"
-                                    labelPlacement="outside"
-                                    placeholder="Email"
-                                />
+                    {getRegistrationStatus() === RegistrationStatus.NOT_REGISTERED && (
+                        <Form className="w-full" validationBehavior="native" onSubmit={onSubmit}>
+                            <div className="col w-full gap-4">
+                                <div className="flex w-full gap-2">
+                                    <Input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        value={email}
+                                        onValueChange={setEmail}
+                                        size="md"
+                                        classNames={{
+                                            inputWrapper: 'bg-[#fcfcfd] border rounded-lg',
+                                            input: 'font-medium rounded-lg',
+                                        }}
+                                        variant="bordered"
+                                        color="primary"
+                                        labelPlacement="outside"
+                                        placeholder="Email"
+                                        disabled={isLoading}
+                                    />
 
-                                <div className="flex">
-                                    <Button color="primary" className="rounded-lg" isLoading={isLoading} type="submit">
-                                        <div className="text-sm font-medium">Register</div>
-                                    </Button>
+                                    <div className="flex">
+                                        <Button color="primary" className="rounded-lg" isLoading={isLoading} type="submit">
+                                            <div className="text-sm font-medium">Register</div>
+                                        </Button>
+                                    </div>
+                                </div>
+
+                                <div className="row gap-2">
+                                    <Switch
+                                        isSelected={isSelected}
+                                        onValueChange={setSelected}
+                                        size="sm"
+                                        disabled={isLoading}
+                                    />
+                                    <div className="text-sm font-medium text-slate-700">
+                                        Subscribe to receive updates on email
+                                    </div>
                                 </div>
                             </div>
+                        </Form>
+                    )}
 
-                            <div className="row gap-2">
-                                <Switch isSelected={isSelected} onValueChange={setSelected} size="sm" />
-                                <div className="text-sm font-medium text-slate-700">Subscribe to receive updates on email</div>
-                            </div>
+                    {getRegistrationStatus() === RegistrationStatus.NOT_CONFIRMED && (
+                        <div>
+                            <Alert
+                                color="warning"
+                                // title="Please confirm your email"
+                                description={
+                                    <div>
+                                        We've sent a confirmation email to{' '}
+                                        <span className="font-semibold">{account.pendingEmail || email}</span>. Please follow
+                                        the link inside the email to confirm your address.
+                                    </div>
+                                }
+                                classNames={{
+                                    base: 'items-center',
+                                }}
+                            />
                         </div>
-                    </Form>
+                    )}
                 </div>
             </Card>
 
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -58,15 +58,15 @@ function Profile() {
     }, [authenticated]);
 
     const getRegistrationStatus = (): RegistrationStatus => {
-        if (account && account.email) {
-            if (account.emailConfirmed) {
-                return RegistrationStatus.REGISTERED;
-            } else {
-                return RegistrationStatus.NOT_CONFIRMED;
-            }
-        } else {
-            return RegistrationStatus.NOT_REGISTERED;
+        if (account && account.email && account.emailConfirmed) {
+            return RegistrationStatus.REGISTERED;
         }
+
+        if (account && account.pendingEmail && !account.emailConfirmed) {
+            return RegistrationStatus.NOT_CONFIRMED;
+        }
+
+        return RegistrationStatus.NOT_REGISTERED;
     };
 
     if (!authenticated) {
@@ -110,7 +110,12 @@ function Profile() {
 
     return (
         <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6">
-            <RegistrationCard account={account} getRegistrationStatus={getRegistrationStatus} />
+            <RegistrationCard
+                account={account}
+                getRegistrationStatus={getRegistrationStatus}
+                fetchAccount={fetchAccount}
+                setAccount={setAccount}
+            />
 
             <KycCard account={account} getRegistrationStatus={getRegistrationStatus} />
 
