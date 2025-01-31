30d4ab85e685a0122054c01a92022c21cf1b4f1e
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-01-31 14:04:54 +0200

    Fix content title styling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -25,22 +25,20 @@ function Content() {
 
     return (
         <div className="col mx-auto h-full max-w-6xl gap-6 px-4 md:gap-8 md:px-8 lg:gap-12 lg:px-10">
-            <div className="flex flex-col-reverse items-center justify-between gap-8 lg:flex-row lg:items-start lg:gap-0">
+            <div className="flex flex-col-reverse items-center justify-between gap-8 lg:gap-8 larger:flex-row larger:items-start">
                 <div className="col gap-2">
                     {!!title && (
-                        <div className="row justify-center lg:h-[40.8px] lg:justify-start">
-                            <div className="text-[28px] font-bold leading-9 lg:text-[30px] larger:text-[34px]">{title}</div>
+                        <div className="row justify-center larger:justify-start">
+                            <div className="text-[28px] font-bold leading-none lg:text-[30px] larger:text-[32px]">{title}</div>
                         </div>
                     )}
 
                     {!!description && (
-                        <div className="text-center text-base leading-5 text-slate-500 lg:text-left lg:text-lg">
-                            {description}
-                        </div>
+                        <div className="text-center text-base text-slate-500 lg:text-left lg:text-lg">{description}</div>
                     )}
                 </div>
 
-                <div className="row w-full justify-between lg:w-auto">
+                <div className="row w-full justify-between larger:w-auto">
                     <div className="mobile-only-block">
                         <img src={Logo} alt="Logo" className="h-10" />
                     </div>
