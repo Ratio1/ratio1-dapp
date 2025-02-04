531e95b86de72929d82f624b2d4947e04afbbddb
Alessandro <defranceschi_a@yahoo.com>
2025-02-04 17:08:48 +0200

    new testnet addresses

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,11 +5,11 @@ import { addSeconds, differenceInSeconds } from 'date-fns';
 
 // ERC20Mock: 0x97b198628cEBB6d8e743fd0015b4cac92A3B1c08
 
-export const r1ContractAddress = '0xBbcbD433Cc666d0Cd11644B6a3954D7C09C0E060';
-export const ndContractAddress = '0x0421b7c9A3B1a4f99F56131b65d15085C7cCACB0';
-export const mndContractAddress = '0xB79fb53ABd43427be6995C194a502bC5AC82D512';
+export const r1ContractAddress = '0x4De05A4705aCd59da041DC70cF989F8111DD414b';
+export const ndContractAddress = '0x2B72b240E74735aF3a731AeD09532320D34b4679';
+export const mndContractAddress = '0x57e5C5409E748807d0ca8a860eCe3F2aCBCcC9B0';
 export const liquidityManagerContractAddress = '0x11Dcb4Fe7B22fd201bcd3da400134e2bf8083f79';
-export const safeAddress = '0xE37562D1Da0F8447bD3cf476906774Cb68501189';
+export const safeAddress = '0x9e7E5655db30E63edfcE83A227571377b07988F1';
 
 export const getContractAddress = (type: 'ND' | 'MND' | 'GND') => {
     switch (type) {
@@ -25,7 +25,7 @@ export const explorerUrl = 'https://sepolia.basescan.org';
 
 export const projectId = '6fb791d3d18d57d28ae7677e4cff8c6e';
 
-export const genesisDate = new Date('2025-01-28T20:00:00.000Z');
+export const genesisDate = new Date('2025-02-04T12:00:00.000Z');
 export const epochDurationInSeconds = 3600; // 1 hour
 
 export const getCurrentEpoch = (): number => Math.floor(differenceInSeconds(new Date(), genesisDate) / epochDurationInSeconds);
@@ -50,4 +50,4 @@ export const wagmiAdapter = new WagmiAdapter({
     ssr: false,
 });
 
-export const ND_LICENSE_CAP = 15_752n * 10n ** 18n;
+export const ND_LICENSE_CAP = 1575_188843457943924200n;
