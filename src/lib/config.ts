0fb1e548e28c7644ea87f49fba49eb48c92bd0f8
Alessandro <defranceschi_a@yahoo.com>
2025-02-05 22:31:26 +0200

    New testnet contracts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,11 +4,11 @@ import { baseSepolia } from '@reown/appkit/networks';
 import { addSeconds } from 'date-fns';
 import { getCurrentEpoch } from './utils';
 
-export const r1ContractAddress = '0x4De05A4705aCd59da041DC70cF989F8111DD414b';
-export const ndContractAddress = '0x2B72b240E74735aF3a731AeD09532320D34b4679';
-export const mndContractAddress = '0x57e5C5409E748807d0ca8a860eCe3F2aCBCcC9B0';
-export const liquidityManagerContractAddress = '0xFDef845f6F2C73064F56b153eDB5b1AA37aaa2a7';
-export const safeAddress = '0x9e7E5655db30E63edfcE83A227571377b07988F1';
+export const r1ContractAddress = '0xCA32aD806BB1e086D77c733656c20334bf2976D6';
+export const ndContractAddress = '0x9aB4e425c7dFFC7Aa1A7a262727b0b663e047571';
+export const mndContractAddress = '0x658bE7d73EBd0Ba7DCe26c112d9871B7Bed890EC';
+export const liquidityManagerContractAddress = '0x7ecC3d8519f068D730EEDeCe899C4CdEC7A4E6A1';
+export const safeAddress = '0xAEEa4E234096b1AcC16c898B6E057Fb0e33F9cC2';
 
 export const getContractAddress = (type: 'ND' | 'MND' | 'GND') => {
     switch (type) {
@@ -24,7 +24,7 @@ export const explorerUrl = 'https://sepolia.basescan.org';
 
 export const projectId = '6fb791d3d18d57d28ae7677e4cff8c6e';
 
-export const genesisDate = new Date('2025-02-04T12:00:00.000Z');
+export const genesisDate = new Date('2025-02-05T17:00:00.000Z');
 export const epochDurationInSeconds = 3600; // 1 hour
 
 // export const getCurrentEpoch = (): number => Math.floor(differenceInSeconds(new Date(), genesisDate) / epochDurationInSeconds);
