652994a84e80b4b297b474b9a3816cc3bc1b95bf
Alessandro <defranceschi_a@yahoo.com>
2025-02-07 00:21:55 +0200

    Mainnet contracts config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -26,16 +26,16 @@ const configs: {
     [key in 'mainnet' | 'testnet' | 'devnet']: Config;
 } = {
     mainnet: {
-        backendUrl: 'https://ratio1-backend.ngrok.app',
-        oraclesUrl: 'https://naeural-oracle.ngrok.app',
-        r1ContractAddress: '0xCA32aD806BB1e086D77c733656c20334bf2976D6',
-        ndContractAddress: '0x9aB4e425c7dFFC7Aa1A7a262727b0b663e047571',
-        mndContractAddress: '0x658bE7d73EBd0Ba7DCe26c112d9871B7Bed890EC',
-        liquidityManagerContractAddress: '0x7ecC3d8519f068D730EEDeCe899C4CdEC7A4E6A1',
-        safeAddress: '0xAEEa4E234096b1AcC16c898B6E057Fb0e33F9cC2',
+        backendUrl: 'https://dapp-api.ratio1.ai',
+        oraclesUrl: 'https://oracle-main.ratio1.ai',
+        r1ContractAddress: '0xc992DcaB6D3F8783fBf0c935E7bCeB20aa50A6f1',
+        ndContractAddress: '0xE20198EE2B76eED916A568a47cdea9681f7c79BF',
+        mndContractAddress: '0xfD52a7958088dF734D523d618e583e4d53cD7420',
+        liquidityManagerContractAddress: '0xTODO',
+        safeAddress: '0x52B56A307469B1f35BC53BF55E53bBf585428Ff4',
         explorerUrl: 'https://basescan.org',
-        genesisDate: new Date('2025-02-05T17:00:00.000Z'),
-        epochDurationInSeconds: 3600,
+        genesisDate: new Date('2025-02-05T16:00:00.000Z'),
+        epochDurationInSeconds: 86400, // 24 hours
         mndCliffEpochs: 120,
         gndVestingEpochs: 365,
         mndVestingEpochs: 900,
