7e27ee2e80dcafc58d95158109a3190d70ddc1fe
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-02-06 17:59:09 +0200

    Display environment

--- src/shared/ApiStatusCard.tsx
+++ src/shared/ApiStatusCard.tsx
@@ -1,7 +1,9 @@
 import { ping } from '@lib/api/backend';
+import { environment } from '@lib/config';
 import { Spinner } from '@nextui-org/spinner';
 import { useQuery } from '@tanstack/react-query';
 import clsx from 'clsx';
+import { RiGlobalLine } from 'react-icons/ri';
 
 function ApiStatusCard() {
     const { data, error, isLoading } = useQuery({
@@ -11,21 +13,28 @@ function ApiStatusCard() {
     });
 
     return (
-        <div className="row mx-auto gap-2 rounded-lg bg-[#e8ebf6] px-3.5 py-2.5">
-            <div className="center-all">
-                {isLoading ? (
-                    <Spinner size="sm" className="scale-75" />
-                ) : (
-                    <div
-                        className={clsx('h-2.5 w-2.5 rounded-full', {
-                            'bg-green-500': !error,
-                            'bg-red-500': data?.status === 'error' || !!error,
-                        })}
-                    ></div>
-                )}
+        <div className="col gap-2">
+            <div className="row mx-auto gap-1.5 rounded-lg bg-[#e8ebf6] px-3.5 py-2.5">
+                <RiGlobalLine className="text-lg text-slate-600" />
+                <div className="text-sm font-medium text-slate-600">{environment}</div>
             </div>
 
-            <div className="text-sm font-medium text-slate-600">API Status</div>
+            <div className="row mx-auto gap-2 rounded-lg bg-[#e8ebf6] px-3.5 py-2.5">
+                <div className="center-all">
+                    {isLoading ? (
+                        <Spinner size="sm" className="scale-75" />
+                    ) : (
+                        <div
+                            className={clsx('h-2.5 w-2.5 rounded-full', {
+                                'bg-green-500': !error,
+                                'bg-red-500': data?.status === 'error' || !!error,
+                            })}
+                        ></div>
+                    )}
+                </div>
+
+                <div className="text-sm font-medium text-slate-600">API Status</div>
+            </div>
         </div>
     );
 }
