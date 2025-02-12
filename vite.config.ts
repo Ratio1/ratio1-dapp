33dccce4fe36f39d05cee3712267815e502958c9
Alessandro <defranceschi_a@yahoo.com>
2025-02-12 22:39:24 +0100

    add back ssl on dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -5,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
     server: { host: '0.0.0.0', port: 3000 },
-    plugins: [react()],
+    plugins: [react(), basicSsl()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
