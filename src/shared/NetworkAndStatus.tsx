5775fd835efc446c1451555c3bf007590c5c5221
wzrdx <128477299+wzrdx@users.noreply.github.com>
2025-06-17 13:58:19 +0300

    Refactor NetworkAndStatus component layout for improved responsiveness and structure, adjusting class names and element hierarchy.

--- src/shared/NetworkAndStatus.tsx
+++ src/shared/NetworkAndStatus.tsx
@@ -12,29 +12,35 @@ function NetworkAndStatus() {
     });
 
     return (
-        <div className="center-all gap-2">
-            <div className="row gap-2 rounded-lg bg-slate-200 px-3.5 py-2.5">
-                <div className="center-all">
-                    {isLoading ? (
-                        <Spinner size="sm" className="scale-75" />
-                    ) : (
-                        <div
-                            className={clsx('h-2.5 w-2.5 rounded-full', {
-                                'bg-green-500': !error,
-                                'bg-red-500': data?.status === 'error' || !!error,
-                            })}
-                        ></div>
-                    )}
+        <div className="col gap-2">
+            <div className="center-all gap-2">
+                <div className="row gap-2 rounded-lg bg-slate-200 px-3.5 py-2.5">
+                    <div className="center-all">
+                        {isLoading ? (
+                            <Spinner size="sm" className="scale-75" />
+                        ) : (
+                            <div
+                                className={clsx('h-2.5 w-2.5 rounded-full', {
+                                    'bg-green-500': !error,
+                                    'bg-red-500': data?.status === 'error' || !!error,
+                                })}
+                            ></div>
+                        )}
+                    </div>
+
+                    <div className="text-sm font-medium text-slate-600">API Status</div>
                 </div>
 
-                <div className="text-sm font-medium text-slate-600">API Status</div>
+                <NetworkSelector />
             </div>
 
-            <NetworkSelector />
-
-            {!!import.meta.env.VITE_APP_VERSION && (
-                <div className="pt-1 text-center text-sm font-medium text-slate-500">v{import.meta.env.VITE_APP_VERSION}</div>
-            )}
+            <div className="mx-auto">
+                {!!import.meta.env.VITE_APP_VERSION && (
+                    <div className="pt-1 text-center text-sm font-medium text-slate-500">
+                        v{import.meta.env.VITE_APP_VERSION}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
