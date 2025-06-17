71d62815baaba51ce09a30b942cfc81c1c16702f
Alessandro <defranceschi_a@yahoo.com>
2025-06-17 11:10:56 +0200

    Try bump (MAJOR)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -181,7 +181,7 @@ export const environment: 'mainnet' | 'testnet' | 'devnet' =
           ? ('devnet' as const)
           : domain === domainTestnet
             ? ('testnet' as const)
-            : ('testnet' as const);
+            : ('devnet' as const);
 
 export const getR1ExplorerUrl = () => `https://${environment === 'mainnet' ? '' : `${environment}-`}${explorerBaseDomain}`;
 
