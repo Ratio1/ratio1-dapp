1234eab4573b09fba435356434a058b56b8271e9
Alessandro <alessandro.defranceschi@dbcodingsrl.com>
2026-04-24 15:57:44 +0300

    fixes

--- src/pages/PlaywrightPreview.tsx
+++ src/pages/PlaywrightPreview.tsx
@@ -1,6 +1,4 @@
-import Tiers from '@components/Tiers';
 import { routePath } from '@lib/routes/route-paths';
-import { PriceTier } from '@typedefs/blockchain';
 import { Navigate } from 'react-router-dom';
 
 /*
